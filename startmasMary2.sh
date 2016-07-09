9f6e95f6181eba66b9154e9cb0cd8cca878f29ba
Agnese Salutari <agneses92@hotmail.it>
2016-07-09 19:36:48 +0200

    Update startmasMary2.sh

--- startmasMary2.sh
+++ startmasMary2.sh
@@ -44,7 +44,7 @@ do
     echo "Agente: $agent_base"
     ./conf/makeconf.sh $agent_base $dali_home
     ./conf/startagent.sh $agent_base $prolog $dali_home > /dev/null &
-    $WAIT > /dev/null && sleep(10) # %WAIT% >nul &
+    $WAIT > /dev/null # %WAIT% >nul &
 done
 
 sleep(150)
